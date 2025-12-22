00ab42bc35effecbca722d112c282a6048d81e8a
zstepanyan <159805567+zstepanyan@users.noreply.github.com>
2025-12-22 23:11:34 +0400

    temp changes

--- src/core/SlideFactory.cpp
+++
@@ -1,5 +0,0 @@
-#include "../../include/core/SlideFactory.hpp"
-
-std::unique_ptr<Slide> SlideFactory::createSlide(int slideNumber) {
-    return std::make_unique<Slide>(slideNumber);
-}
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/mvc/Controller.hpp"
-
-int main() {
-    Controller controller;
-    controller.run();
-    return 0;
-}
